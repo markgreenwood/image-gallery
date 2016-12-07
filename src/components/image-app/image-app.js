fe1400169db82cd0af5871ec0d5934e8974fe133
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-12-07 07:51:41 -0800

    More formatting, changed image link and info

--- src/components/image-app/image-app.js
+++ src/components/image-app/image-app.js
@@ -8,9 +8,9 @@ export default {
 
 function controller() {
   this.img = {
-    title: 'Cute Bunny',
-    link: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg',
-    description: 'Really, really cute bunny'
+    title: 'David Zabriskie Time Trial',
+    link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Dave_Zabriskie_-_USA_Pro_Time_Trial.jpg/1280px-Dave_Zabriskie_-_USA_Pro_Time_Trial.jpg?1481125420015',
+    description: 'An extremely fast time trialist, David Zabriskie has clocked an average speed of 54.676 km/h (34 mph) in Tour de France time trials.'
   };
 
   this.tabs = [ 'all', 'details', 'thumbnail', 'full' ];

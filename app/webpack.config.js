5122ac211d79279b3ee9b6c318ded1fea7fd6272
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-12-12 14:42:26 -0800

    Changed webpack config to build to /server/public for deployment

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: './build',
+    path: '../server/public',
     filename: 'app.js'
   },
   plugins: [

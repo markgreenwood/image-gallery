c53678becd2f8befc97aeb7a89f53c63808de18d
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-12-12 14:53:11 -0800

    Changed to SCSS implementation

---
+++ app/src/main.js
@@ -0,0 +1,10 @@
+import angular from 'angular';
+import './scss/main.scss';
+import components from './components';
+import services from './services';
+
+const app = angular.module('myApp', [ components, services ]);
+
+const dev = 'http://localhost:3000/api';
+app.value('apiUrl', dev);
+app.factory(function() { return dev; });

bf3254f2d006a3dd6fc1fe687083a0a6c2a03315
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-12-19 23:11:34 -0800

    Improved formatting

--- app/src/components/welcome/welcome.js
+++ app/src/components/welcome/welcome.js
@@ -3,5 +3,5 @@
 
 export default {
   template: '<h2>Welcome to the Image Gallery...</h2>' + 
-    '<p>...a facility for storing references to your favorite images. The image gallery uses Angular for the frontend and Node.js, Express, and Mongo DB for the backend.'
+    '<p>...a facility for storing references to your favorite images. The image gallery uses AngularJS for the frontend and Node.js, Express, and Mongo DB for the backend.'
 };
